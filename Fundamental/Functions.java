004723ecd383e7092049c94f4dc4fc75b305275b
Michael Anggi Gilang Angkasa <63223847+Michael-Obs66@users.noreply.github.com>
2025-09-17 15:03:22 +0700

    Create Functions.java

---
+++ Fundamental/Functions.java
@@ -0,0 +1,10 @@
+public class Functions {
+    public static void main(String[] args) {
+        int result = add(7, 3);
+        System.out.println("7 + 3 = " + result);
+    }
+
+    public static int add(int x, int y) {
+        return x + y;
+    }
+}
