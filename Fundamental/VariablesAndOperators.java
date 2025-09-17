47514f95626e52b0c1dd3bc31dc7a4268b47e623
Michael Anggi Gilang Angkasa <63223847+Michael-Obs66@users.noreply.github.com>
2025-09-17 15:02:21 +0700

    Create VariablesAndOperators.java

---
+++ Fundamental/VariablesAndOperators.java
@@ -0,0 +1,13 @@
+public class VariablesAndOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        int diff = a - b;
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + diff);
+
+        boolean flag = (a > b);
+        System.out.println("Is a greater than b? " + flag);
+    }
+}
